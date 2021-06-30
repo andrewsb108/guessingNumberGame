fc2bea63ca109d395a24610469669053378518db
andrewsb108 <andrey.barsuk1@gmail.com>
2021-06-30 11:32:37 +0300

    Created testClass ModelTest

---
+++ src/test/java/practice3/ModelTest.java
@@ -0,0 +1,41 @@
+package practice3;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void setSecretValue() {
+    }
+
+    @Test
+    public void checkValue() {
+    }
+
+    @Test
+    public void setPrimaryBarrier() {
+    }
+
+    @Test
+    public void getMaxBarrier() {
+    }
+
+    @Test
+    public void getMinBarrier() {
+    }
+
+    @Test
+    public void getSecretValue() {
+    }
+
+    @Test
+    public void getYourWay() {
+    }
+}
