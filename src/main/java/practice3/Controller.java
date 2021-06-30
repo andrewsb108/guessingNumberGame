0be6f4c715ff49f756f4a993593b8ebd48062242
andrewsb108 <andrey.barsuk1@gmail.com>
2021-06-30 10:09:25 +0300

    Initialization in Main

--- src/main/java/practice3/Controller.java
+++ src/main/java/practice3/Controller.java
@@ -1,4 +1,13 @@
 package practice3;
 
 public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View  view) {
+        this.model = model;
+        this.view = view;
+    }
+
+
 }
--- src/main/java/practice3/Main.java
+++ src/main/java/practice3/Main.java
@@ -1,5 +1,7 @@
 package practice3;
 
 public class Main {
+    Controller controller = new Controller(new Model(), new View());
 
+    controller.startGame();
 }
